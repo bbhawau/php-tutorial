370aec498022d66eb11d53cfb60025a684e41c56
hawau <hawaubaraje@gmail.com>
2024-12-04 14:32:31 +0100

    modified:   index.php

--- index.php
+++ index.php
@@ -180,4 +180,61 @@ echo "<br>";
 // round()  -- Rounds a floating-point number to the nearest integer.
 echo round(4.7); // Output: 5
 echo "<br>";
+
+
+// functions
+
+function greet($firstname, $lastname, $age) {
+
+    return "Hello, ". $firstname . "" . $lastname . "" . $age;
+}
+
+$message = greet("Alex", "fred", 32);
+
+echo $message;
+
+
+
+function add($a, $b, $c){
+    return $a + $b - $c;
+}
+
+$sum  =  add(4, 6, 3);
+echo "The sum is: ". $sum ;
+
+
+//  statements
+$limits = 400;
+$limited = 250;
+$isPassed = true;
+$names = "fred";
+
+if(!!$isPassed){
+    echo "ok";
+}
+ else {
+    echo "Error";
+}
+
+
+function statem($a, $b, $c){
+
+    if($a > $b){
+
+        if($a > $b){
+
+            return "greater";
+        } elseif(premid) {
+            return "lesser";
+        } else{
+            jkjgjfj
+        }
+    } else {
+        return "lesser";
+    }
+}
+
+$checks = statem(3,5,9);
+
+echo $checks;
 ?>

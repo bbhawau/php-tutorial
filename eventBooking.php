fe73d967a263b3f6bacb282460f3a9f2f7fa4dfc
hawau <hawaubaraje@gmail.com>
2024-12-20 23:00:51 +0100

    new file:   eventBooking.php

---
+++ eventBooking.php
@@ -0,0 +1,10 @@
+<?php
+//register with email and password
+//login with email and password
+//create event with: title, description, date, time, location, available seats
+//booking seats; logic for overbooking
+//users: event organizers & regular users
+// requirements: user authentication, data validation, logic for datat integrity
+
+
+?>
